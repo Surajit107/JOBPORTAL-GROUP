8d33eec7d9f840fa994d56843da802787d81d83a
Debarati Das <debarati84430@gmail.com>
2022-11-29 02:54:30 +0530

    readaboutus and readourblogs text updated

--- src/components/core/home/ReadAboutUs.jsx
+++ src/components/core/home/ReadAboutUs.jsx
@@ -1,43 +1,61 @@
-import React from 'react'
+import React from "react";
 
 const ReadAboutUs = () => {
-    return (
-        <div>
-            {/* <!-- ***** Start Read About Us ***** --> */}
+  return (
+    <div>
+      {/* <!-- ***** Start Read About Us ***** --> */}
 
-            <section className="section section-bg" id="schedule"
-                style={{"backgroundImage": "url(assets/images/about-fullscreen-1-1920x700.jpg)"}}>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-6 offset-lg-3">
-                            <div className="section-heading dark-bg">
-                                <h2>Read <em>About Us</em></h2>
-                                <img src="assets/images/line-dec.png" alt=""/>
-                                    <p>Nunc urna sem, laoreet ut metus id, aliquet consequat magna. Sed viverra ipsum dolor,
-                                        ultricies fermentum massa consequat eu.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="cta-content text-center">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore deleniti voluptas enim!
-                                    Provident consectetur id earum ducimus facilis, aspernatur hic, alias, harum rerum velit
-                                    voluptas, voluptate enim! Eos, sunt, quidem.</p>
-
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto nulla quo cum officia
-                                    laboriosam. Amet tempore, aliquid quia eius commodi, doloremque omnis delectus laudantium
-                                    dolor reiciendis non nulla! Doloremque maxime quo eum in culpa mollitia similique eius
-                                    doloribus voluptatem facilis! Voluptatibus, eligendi, illum. Distinctio, non!</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            {/* <!-- ***** End Read About Us ***** --> */}
+      <section
+        className="section section-bg"
+        id="schedule"
+        style={{
+          backgroundImage: "url(assets/images/about-fullscreen-1-1920x700.jpg)",
+        }}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 offset-lg-3">
+              <div className="section-heading dark-bg">
+                <h2>
+                  Read <em>About Us</em>
+                </h2>
+                <img src="assets/images/line-dec.png" alt="" />
+                <p>
+                  we have secured our position as leaders in international
+                  recruitment and hiring. We currently have 139 offices that
+                  create a network that spans 37 countries around the world,
+                  with strong opportunities for more growth within Asia Pacific.
+                  We opened our first office in Asia in 1994, and have developed
+                  our presence into many of the main business hubs in the region
+                  since then. Our team in Asia has global connections, regional
+                  knowledge and local market experience; ideally positioning us
+                  to partner with multinational blue-chip companies and local
+                  firms alike, and with todays global professionals. Over time,
+                  our founding mission has remained unchanged: to bring the
+                  worlds best companies and brightest professionals together,
+                  helping each side reach their maximum potential and
+                  capabilities.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="cta-content text-center">
+                <p>
+                  The quality of our service is demonstrated by the fact that
+                  over 90% of our current business comes from referrals and
+                  repeat clients.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </section>
 
-export default ReadAboutUs
+      {/* <!-- ***** End Read About Us ***** --> */}
+    </div>
+  );
+};
+
+export default ReadAboutUs;
