fb981340b914486205b448258f720190c89f96b3
Surajit Pal <palsurajit06@gmail.cpm>
2022-11-29 22:34:41 +0530

    PROJECT COMPLETED

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import Banner from "../components/common/banners/Banner";
+import React from 'react'
+import Banner from '../components/common/banners/Banner'
 
 const Team = () => {
   return (
     <div>
-      <Banner string="Meet our" page="Team" />
+      <Banner string="Meet our" page="Team"/>
 
       <section className="section" id="trainers">
         <div className="container">
@@ -14,33 +14,17 @@ const Team = () => {
             <div className="col-md-3 col-sm-6">
               <div className="trainer-item">
                 <div className="image-thumb">
-                  <img src="/assets/images/team1.jpg" height="300" width="400" alt="" />
+                  <img src="assets/images/team-image-1-646x680.jpg" alt="" />
                 </div>
                 <div className="down-content">
                   <span>CEO</span>
-                  <h4>Surajit Paul</h4>
-
+                  <h4>John Doe</h4>
+                  <p>Vestibulum id est eu felis vulputate hendrerit. Suspendisse dapibus turpis in dui pulvinar imperdiet. Nunc consectetur.</p>
                   <ul className="social-icons">
-                    <li>
-                      <a href="!#">
-                        <i className="fa fa-facebook"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="!#">
-                        <i className="fa fa-twitter"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="!#">
-                        <i className="fa fa-linkedin"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="!#">
-                        <i className="fa fa-behance"></i>
-                      </a>
-                    </li>
+                    <li><a href="!#"><i className="fa fa-facebook"></i></a></li>
+                    <li><a href="!#"><i className="fa fa-twitter"></i></a></li>
+                    <li><a href="!#"><i className="fa fa-linkedin"></i></a></li>
+                    <li><a href="!#"><i className="fa fa-behance"></i></a></li>
                   </ul>
                 </div>
               </div>
@@ -48,33 +32,17 @@ const Team = () => {
             <div className="col-md-3 col-sm-6">
               <div className="trainer-item">
                 <div className="image-thumb">
-                  <img src="assets/images/team4.jpg" height="300" width="400" alt="" />
+                  <img src="assets/images/team-image-2-646x680.jpg" alt="" />
                 </div>
                 <div className="down-content">
                   <span>Customer Support</span>
-                  <h4>Sohini Choudhury</h4>
-
+                  <h4>Jane Smith</h4>
+                  <p>Vestibulum id est eu felis vulputate hendrerit. Suspendisse dapibus turpis in dui pulvinar imperdiet. Nunc consectetur.</p>
                   <ul className="social-icons">
-                    <li>
-                      <a href="!#">
-                        <i className="fa fa-facebook"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="!#">
-                        <i className="fa fa-twitter"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="!#">
-                        <i className="fa fa-linkedin"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="!#">
-                        <i className="fa fa-behance"></i>
-                      </a>
-                    </li>
+                    <li><a href="!#"><i className="fa fa-facebook"></i></a></li>
+                    <li><a href="!#"><i className="fa fa-twitter"></i></a></li>
+                    <li><a href="!#"><i className="fa fa-linkedin"></i></a></li>
+                    <li><a href="!#"><i className="fa fa-behance"></i></a></li>
                   </ul>
                 </div>
               </div>
@@ -82,33 +50,17 @@ const Team = () => {
             <div className="col-md-3 col-sm-6">
               <div className="trainer-item">
                 <div className="image-thumb">
-                  <img src="assets/images/team3.jpg" height="300" width="400"  alt="" />
+                  <img src="assets/images/team-image-3-646x680.jpg" alt="" />
                 </div>
                 <div className="down-content">
                   <span>Customer Support</span>
-                  <h4>Monalisa Banerjee</h4>
-
+                  <h4>Kate Johnson</h4>
+                  <p>Vestibulum id est eu felis vulputate hendrerit. Suspendisse dapibus turpis in dui pulvinar imperdiet. Nunc consectetur.</p>
                   <ul className="social-icons">
-                    <li>
-                      <a href="!#">
-                        <i className="fa fa-facebook"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="!#">
-                        <i className="fa fa-twitter"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="!#">
-                        <i className="fa fa-linkedin"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="!#">
-                        <i className="fa fa-behance"></i>
-                      </a>
-                    </li>
+                    <li><a href="!#"><i className="fa fa-facebook"></i></a></li>
+                    <li><a href="!#"><i className="fa fa-twitter"></i></a></li>
+                    <li><a href="!#"><i className="fa fa-linkedin"></i></a></li>
+                    <li><a href="!#"><i className="fa fa-behance"></i></a></li>
                   </ul>
                 </div>
               </div>
@@ -117,33 +69,17 @@ const Team = () => {
             <div className="col-md-3 col-sm-6">
               <div className="trainer-item">
                 <div className="image-thumb">
-                  <img src="assets/images/team2.jpg" height="300" width="400"  alt="" />
+                  <img src="assets/images/team-image-4-646x680.jpg" alt="" />
                 </div>
                 <div className="down-content">
                   <span>Customer Support</span>
-                  <h4>Sagar Banik</h4>
-
+                  <h4>Mark Green</h4>
+                  <p>Vestibulum id est eu felis vulputate hendrerit. Suspendisse dapibus turpis in dui pulvinar imperdiet. Nunc consectetur.</p>
                   <ul className="social-icons">
-                    <li>
-                      <a href="!#">
-                        <i className="fa fa-facebook"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="!#">
-                        <i className="fa fa-twitter"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="!#">
-                        <i className="fa fa-linkedin"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="!#">
-                        <i className="fa fa-behance"></i>
-                      </a>
-                    </li>
+                    <li><a href="!#"><i className="fa fa-facebook"></i></a></li>
+                    <li><a href="!#"><i className="fa fa-twitter"></i></a></li>
+                    <li><a href="!#"><i className="fa fa-linkedin"></i></a></li>
+                    <li><a href="!#"><i className="fa fa-behance"></i></a></li>
                   </ul>
                 </div>
               </div>
@@ -151,8 +87,9 @@ const Team = () => {
           </div>
         </div>
       </section>
-    </div>
-  );
-};
 
-export default Team;
+    </div>
+  )
+}
+
+export default Team
